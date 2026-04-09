e37d02284ed9559e32f797b74f8d0d62609f5508
GonesDev <lugdunum69330@gmail.com>
2026-04-09 21:21:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// LOADER
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.transition = "0.5s";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 1200);
+
+  // afficher les cards dès le chargement
+  document.querySelectorAll(".card").forEach(card => {
+    card.classList.add("show");
+  });
+});
+
+
+// ANIMATION AU SCROLL
 const cards = document.querySelectorAll(".card");
 
 window.addEventListener("scroll", () => {
