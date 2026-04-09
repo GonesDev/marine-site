86fd506b546111b9085142995e19720b8fb18f16
GonesDev <lugdunum69330@gmail.com>
2026-04-09 21:50:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-// LOADER SAFE
+// AFFICHER LES CARTES AU CHARGEMENT
 window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-
-  if (loader) {
-    setTimeout(() => {
-      loader.style.opacity = "0";
-      loader.style.transition = "0.5s";
-
-      setTimeout(() => {
-        loader.style.display = "none";
-      }, 500);
-    }, 1200);
-  }
-
-  // afficher les cards
   document.querySelectorAll(".card").forEach(card => {
     card.classList.add("show");
   });
 });
 
 
-// ANIMATION SCROLL
+// ANIMATION AU SCROLL
 const cards = document.querySelectorAll(".card");
 
 window.addEventListener("scroll", () => {
